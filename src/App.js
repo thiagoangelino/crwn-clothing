dbb7641a4151f4457156567ceb890cdb231034c6
Thiago Angelino <thiagoangelinos@gmail.com>
2020-06-29 20:32:28 -0300

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+)
+const JacketsPage = () => (
+  <div>
+    <h1> JACKETS PAGE </h1>
+  </div>
+)
+const SneakersPage = () => (
+  <div>
+    <h1> SNEAKERS PAGE </h1>
+  </div>
+)
+const WomensPage = () => (
+  <div>
+    <h1> WOMENS PAGE </h1>
+  </div>
+)
+const MensPage = () => (
+  <div>
+    <h1> MENS PAGE </h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+        <Route exact path='/shop/jackets' component={JacketsPage}/>
+        <Route exact path='/shop/sneakers' component={SneakersPage}/>
+        <Route exact path='/shop/womens' component={WomensPage}/>
+        <Route exact path='/shop/mens' component={MensPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import MenuItem from '../menu-item/menu-item.component'
+
+
 import '../directory/directory.styles.scss'
 
 class Directory extends React.Component {
@@ -49,8 +51,8 @@ class Directory extends React.Component {
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) =>(
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherSectionProps}) =>(
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
