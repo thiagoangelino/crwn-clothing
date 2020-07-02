6dd0488cb59c5a8bb137c1a827b032c05933a830
Thiago Angelino <thiagoangelinos@gmail.com>
2020-07-02 11:08:42 -0300

    updating app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,43 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-)
-const JacketsPage = () => (
-  <div>
-    <h1> JACKETS PAGE </h1>
-  </div>
-)
-const SneakersPage = () => (
-  <div>
-    <h1> SNEAKERS PAGE </h1>
-  </div>
-)
-const WomensPage = () => (
-  <div>
-    <h1> WOMENS PAGE </h1>
-  </div>
-)
-const MensPage = () => (
-  <div>
-    <h1> MENS PAGE </h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component.jsx'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop/hats' component={HatsPage}/>
-        <Route exact path='/shop/jackets' component={JacketsPage}/>
-        <Route exact path='/shop/sneakers' component={SneakersPage}/>
-        <Route exact path='/shop/womens' component={WomensPage}/>
-        <Route exact path='/shop/mens' component={MensPage}/>
+        <Route exact path='/shop' component={ShopPage}/>
+        
       </Switch>
     </div>
   );
