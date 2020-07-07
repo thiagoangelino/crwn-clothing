9d4db62455671d062f207c34f58be58d600215c8
Thiago Angelino <thiagoangelinos@gmail.com>
2020-07-07 12:07:43 -0300

    updating app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-import SingInAndSingUp from './pages/sing-in-and-sing-up/sing-in-and-sing-up.component'
+import SingInAndSingUpPage from './pages/sing-in-and-sing-up/sing-in-and-sing-up.component'
 import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils' 
 import { setCurrentUser } from './redux/user/user.actions'
@@ -17,6 +17,7 @@ class App extends React.Component {
 
   componentDidMount(){
     const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth =>{
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
@@ -45,16 +46,25 @@ class App extends React.Component {
                 <Switch>
                   <Route exact path='/' component={HomePage}/>
                   <Route exact path='/shop' component={ShopPage}/>
-                  <Route exact path='/singin' component={SingInAndSingUp}/>
-                  
+                  <Route exact path='/singin' render = {() =>
+                    this.props.currentUser ? (
+                      <Redirect to='/' />
+                    ): (
+                      <SingInAndSingUpPage />
+                    ) } 
+                  />
                 </Switch>
               </div>
             );
   }
 }
 
+const mapStateProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispathToProps = dispath => ({
   setCurrentUser: user => dispath(setCurrentUser(user))
 })
 
-export default connect(null, mapDispathToProps)(App);
+export default connect(mapStateProps, mapDispathToProps)(App);
